120acda160160056796cad34bae432c7b0a1ce23
rs1705 <asmrahul17@gmail.com>
2025-08-03 23:55:35 +0530

    - Updated add-product.html to include css classes for improved styling. - Enhanced not_found.html with a consistent header and styling. - Improved shop.html layout and styling for better user experience. - Created main.css for global styles and product.css for product-specific styles.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
