dca47da2030ebae99945337487a0002467f1f10e
rs1705 <asmrahul17@gmail.com>
2025-08-03 18:21:38 +0530

    created basic routing using express

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 
 const app = express();
 const port = 3000;
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
-  res.send("<h1>Add products</h1>");
+  res.send(
+    '<form action="/products" method="POST"><input type="text" name="title"/><button type="submit">Add product</button></form>'
+  );
+});
+
+app.post("/products", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  res.send("<h1>Welcome from Express->node framework</h1>");
+  res.send("<h1>Homepage</h1>");
 });
 
 app.listen(port, () => {
